87ab50d0414dae07a6184da704e55ebbbb7f821d
kelche <kelcheone@gmail.com>
2022-10-30 08:10:10 +0300

    feat: most features

---
+++ background.js
@@ -0,0 +1,29 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.action.setBadgeText({ text: "ON" });
+  //   set the default state to ON
+  chrome.storage.sync.set({ state: "ON" }, () => {
+    console.log("State is set to ON");
+  });
+});
+
+yt = "https://www.youtube.com";
+
+chrome.action.onClicked.addListener(async (tab) => {
+  console.log(tab.url);
+  if (tab.url.startsWith("https://www.youtube.com")) {
+    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+    const nextState = prevState === "ON" ? "OFF" : "ON";
+
+    // Set the action badge to the next state
+    await chrome.action.setBadgeText({
+      tabId: tab.id,
+      text: nextState,
+    });
+    chrome.storage.sync.set({ state: nextState }, () => {
+      console.log("State is set to " + nextState);
+    });
+
+    const port = chrome.tabs.connect(tab.id);
+    port.postMessage(nextState);
+  }
+});
---
+++ scripts/content.js
@@ -0,0 +1,49 @@
+let state = "ON";
+// get state from storage
+chrome.storage.sync.get(["state"], (result) => {
+  // check if state is set
+  if (result.state) {
+    state = result.state;
+  } else {
+    // if not set, set state to ON
+    chrome.storage.sync.set({ state: "ON" }, () => {
+      console.log("State is set to ON");
+    });
+  }
+});
+
+const hideThumbnails = () => {
+  // hide the thumbnails
+  const thumbnails = document.getElementsByTagName("ytd-thumbnail");
+  for (thumb of thumbnails) {
+    thumb.style.display = "none";
+  }
+};
+
+let intervalId = null;
+
+// only run if the extension is ON
+if (state === "ON") {
+  //  hide the thumbnails on page load and when new videos are loaded
+  window.onload = hideThumbnails;
+  intervalId = setInterval(hideThumbnails, 1000);
+}
+
+// Receive next state from background script via port
+chrome.runtime.onConnect.addListener((port) => {
+  port.onMessage.addListener((nextState) => {
+    // Check if the extension is 'ON' or 'OFF'
+    state = nextState;
+    // If the extension is 'ON', we run the script
+    if (state === "ON") {
+      intervalId = setInterval(hideThumbnails, 1000);
+    } else {
+      clearInterval(intervalId);
+      // If the extension is 'OFF', restore the thumbnails
+      const thumbDiv = document.getElementsByTagName("ytd-thumbnail");
+      for (thumb of thumbDiv) {
+        thumb.style.display = "block";
+      }
+    }
+  });
+});
